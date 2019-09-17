d2c1fc6c1ff7eeb112417021ea82ef3aa2d74b92
haseo <haseo@192.168.219.137>
2019-09-17 09:09:04 +0900

    프로젝트 재생성

---
+++ syj/src/vo/Comment.java
@@ -0,0 +1,19 @@
+package vo;
+
+public class Comment {
+	
+	private int no;
+	private Post post;
+	private User user;
+	private String content;
+	
+	public int getNo() { return no; }
+	public Post getPost() { return post; }
+	public User getUser() { return user; }
+	public String getContent() { return content; }
+	public void setNo(int no) { this.no = no; }
+	public void setPost(Post post) { this.post = post; }
+	public void setUser(User user) { this.user = user; }
+	public void setContent(String content) { this.content = content; }
+	
+} //class Comment;
