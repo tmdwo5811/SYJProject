eb7e27faad8f2d37bcff17ea9eca40a753cfc8d9
tmdwo5811 <tmdwo5811@naver.com>
2019-09-23 17:48:21 +0900

    로그인, 회원가입 폼 jQuery효과 추가 및 css 정리 [윤승재]

--- syj/WebContent/js/script.js
+++ syj/WebContent/js/script.js
@@ -1,3 +1,9 @@
+function maxLengthCheck(object){
+  if (object.value.length > object.maxLength){
+    object.value = object.value.slice(0, object.maxLength);
+  }
+}
+
 (function() {
     var startingTime = new Date().getTime();
     // Load the script
@@ -19,14 +25,12 @@
 checkReady(function($) {
 
     $(function() {
-
-
       $('.login_button').click(function(){ //로그인 버튼 클릭 이벤트
         $('.l_form').slideToggle();
         $('.j_form').slideUp();
       })
       $('.join_button').click(function(){ //회원가입 버튼 클릭 이벤트
-        $('.j_form').slideToggle();
+        $('.j_form, .j_menual').slideToggle();
         $('.l_form').slideUp();
       })
       $('.j_textbox').click(function(){
@@ -34,6 +38,9 @@ checkReady(function($) {
       })
       $('.l_textbox').click(function(){
         $(this).val('').focus()
+      })
+      $('.loc_p_li').click(function(){ //지역청원 카테고리 클릭 이벤트
+        $('.loc_p_li span').animate({'right':1000},300);
       })
       var ljtextbox = $('.j_textbox, .l_textbox');
       ljtextbox.on('focus',function(){
@@ -43,6 +50,12 @@ checkReady(function($) {
         if($(this).val()==""){
           $(this).siblings('label').fadeIn('fast');
         }
+      })
+      var selectTarget = $('.selectbox select');
+      selectTarget.change(function(){
+        var select_name = $(this).children('option:selected').text();
+        $(this).siblings('label').text(select_name);
+        $(this).siblings('label').css('color','#000');
       })
     });//jQuery end
 });
