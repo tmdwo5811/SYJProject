c072b82200202a9d9fb733d2be81a0aef8ec08e6
Koh,Yongajae <roykoh88@naver.com>
2019-09-24 08:41:15 +0900

    asdf

---
+++ syj/src/action/AnswerAction.java
@@ -0,0 +1,15 @@
+package action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AnswerAction implements CommandAction {
+
+	@Override
+	public String requestPro(HttpServletRequest request, HttpServletResponse response) throws Throwable {
+		// TODO Auto-generated method stub
+		return null;
+	}
+//DAO 데이터 가져오기
+//view로 보내주기
+}
