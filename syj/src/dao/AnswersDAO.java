4b4a8a7d1303d9409d5029c21477b38e8db0c80a
JiannKim <jiahnkimm@gmail.com>
2019-10-08 10:16:59 +0900

    AnswerDAO 전반 수정 완료
    sql구문 수정하였음.
    불필요 주석삭제

--- syj/src/dao/AnswersDAO.java
+++ syj/src/dao/AnswersDAO.java
@@ -15,12 +15,12 @@ public class AnswersDAO {
 	
 	public void insertArticle(Answer article) {//~(MemberDTO mem)
 		//1. article=> 신규글인지 답글인지 확인
-		int no=article.getNo();//꺼내온 값이 0이라면 신규글 아니라면 답글
+		int no=article.getNo();//꺼내온 값이 0이라면 신규
 		Post post=article.getPost();
 		String content=article.getContent();
 		Timestamp regdate=article.getRegdate();
 		//테이블에 입력할 게시물번호를 저장할 변수
-		int number=0;//데이터를 저장하기 위한 게시물번호 
+		int num=0;//데이터를 저장하기 위한 게시물번호
 		System.out.println("insertArticle 메소드의 내부no=>"+no);//0=>신규 !=답글
 		System.out.println("post=>"+post);
 		System.out.println("content=>"+content);
@@ -28,42 +28,21 @@ public class AnswersDAO {
 	      try{
 	         con=pool.getConnection();
 	         System.out.println("con=>"+con);//디버깅 코드
-	         sql="select count(*) from board";//최대값+1=실제 저장할 게시물번호
+	         sql="select count(*) from answer";//최대값+1=실제 저장할 답변게시물번호
 	         pstmt=con.prepareStatement(sql);
 	         rs=pstmt.executeQuery();
 	         if(rs.next()) {//보여주는 결과가 있다면
-	            number = rs.getInt(1)+1; //변수명=rs.get자료형(필드명 또는 인덱스 번호)=> 필드명  X를 사용할수 없는경우
+	            num = rs.getInt(1)+1; //변수명=rs.get자료형(필드명 또는 인덱스 번호)=> 필드명  X를 사용할수 없는경우
 	         }else {//현재 테이블에 데이터가 한개라도 없는경우
-	        	 number=1;
+	        	 num=1;
 	         }
 	         
-	         //답글이라면
-			/*
-			 * if(no!=0) {//양수면서 1이상 //같은 그룹번호를 가지고 있으면서 나보다 step값이 큰것을 찾아서, 그 step값을 하나
-			 * 증가시킨다. sql="update board set re_step=re_step+1 where ref=? and re_step > ?";
-			 * pstmt=con.prepareStatement(sql); pstmt.setInt(1, no); pstmt.setInt(2, post);
-			 * int update=pstmt.executeUpdate();
-			 * System.out.println("댓글수정 유무(update)=>"+update);//1이면 성공
-			 * 
-			 * re_step=re_step+1; re_level=re_level+1; }else {//신규글이라면 => no=0
-			 * ref=number;//1 re_step=0; re_level=0; }
-			 */
+	         sql = "INSERT INTO answers(post_no, user_no, content) VALUES(?, ?, ?)";
+
+	         pstmt.setInt(1, article.getPost().getNo()); //일반게시물의 번호
+	         pstmt.setString(2, article.getId().getId()); //답변게시물의 작성자
+	         pstmt.setString(3, article.getContent()); //답변게시물의 글내용
 	         
-	         //12개 데이터->num,reg_date,readcount(생략) ->default | 작성날짜->regdate, now() <--mysql(? 대신에)
-	         sql="insert into board(no,content,regdate,ref,re_step,re_level,content)values(?,?,?,?,?,?,?,?,?,?)";
-	         pstmt=con.prepareStatement(sql);
-	         pstmt.setInt(1, article.getNo());//웹에선 Setter Method를 메모리에 저장한다.
-	         pstmt.setString(2, article.getEmail());
-	         pstmt.setString(3, article.getSubject());
-	         pstmt.setString(4, article.getPasswd());
-	         pstmt.setTimestamp(5, article.getReg_date());//?대신에 now()(웹상에서 계산하여 저장)
-	         //--------ref,re_step,re_level에 대한 계산이 적용된 상태에서 저장----------
-	         pstmt.setInt(6, ref);//pstmt.setInt(6, article.getRef()); (X)
-	         pstmt.setInt(7, re_step);
-	         pstmt.setInt(8, re_level);
-	         //--------------------------------------
-	         pstmt.setString(9, article.getContent());
-	         pstmt.setString(10, article.getIp());//request.getRemoteAddr() | jspf(writePro.jsp)
 	         int insert=pstmt.executeUpdate();
 	         System.out.println("게시판의 글쓰기 성공유무(insert)=>"+insert);//1이면 성공 0 실패
 	      }catch(Exception e) {
