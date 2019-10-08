926025a619991aa5b5f5022029c4eee2fcb4057a
haseo <haseo@192.168.219.137>
2019-10-08 17:17:48 +0900

    용재형 공부파일
    스프링 안올리던 이것저것

--- SYJSpring/src/controller/MainController.java
+++ SYJSpring/src/controller/MainController.java
@@ -2,24 +2,11 @@ package controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MainController {
 	
-	
-//	@Autowired
-//	private UsersService usersService;
-	
 	@RequestMapping("/")
-	public ModelAndView main() {
-		
-		ModelAndView model = new ModelAndView("main");
-		
-		
-		
-		return model;
-		
-	} //main();
+	public void main() {}
 	
 } //class MainController;
