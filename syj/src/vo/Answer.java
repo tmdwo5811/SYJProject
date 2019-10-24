ae2ea1b843f0871c2d77e98231a5d5b08bd43ac0
JiannKim <jiahnkimm@gmail.com>
2019-10-24 09:39:30 +0900

    Answer.java
    user -> id 수정

--- syj/src/vo/Answer.java
+++ syj/src/vo/Answer.java
@@ -5,15 +5,15 @@ import java.sql.Timestamp;
 
 public class Answer {
 	
-	private int no;	//글 번호
-	private Post post;	//게시판의 게시물
-	private User user;	//작성자
-	private String content;	//글내용
-	private Timestamp regdate;	//작성일
+	private int no;	//답변글 번호
+	private Post post;	//일반게시판의 게시물번호
+	private _Login id;	//답변글의 작성자
+	private String content;	//답변 글내용
+	private Timestamp regdate;	//답변글 작성일
 	
 	public int getNo() { return no; }
 	public Post getPost() { return post; }
-	public User getUser() { return user; }
+	public _Login getId() { return id; }
 	public String getContent() { return content; }
 	public Timestamp getRegdate() { return regdate; }
 	public void setNo(int no) { this.no = no; }
