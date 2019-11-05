abdb5868deb15f68eab0e71b4be7370c2c95b6a7
tmdwo5811 <tmdwo5811@naver.com>
2019-11-05 09:09:51 +0900

    로그인 기능 구현 완료 [승재]

--- syj/src/vo/Location.java
+++ syj/src/vo/Location.java
@@ -4,12 +4,15 @@ public class Location extends Vo {
 	
 	private int no;
 	private String name;
-
+//	
+//	public Location(int no) {
+//		this.no = no;
+//	} //Location();
+	//public Location() {}
 	public Location() {}
-	public Location(int no) {
-		this.no = no;
-	} //Location();
-	
+	public Location(int i) {
+		// TODO Auto-generated constructor stub
+	}
 	public int getNo() { return no; }
 	public String getName() { return name; }
 	public void setNo(int no) { this.no = no; }
